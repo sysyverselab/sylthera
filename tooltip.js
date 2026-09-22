d3253ce2f876c860a24ce984ba080bc6005cf4b4
sysyverselab <syrrine.khaldi@gmail.com>
2026-09-22 22:15:30 +0200

    Add tooltip functionality for profile icons

---
+++ tooltip.js
@@ -0,0 +1,26 @@
+(function () {
+  document.querySelectorAll('.profile-icon-btn[title]').forEach(function (el) {
+    var t = el.getAttribute('title');
+    if (t) {
+      el.setAttribute('data-tooltip', t);
+      el.removeAttribute('title');
+    }
+  });
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (m) {
+      m.addedNodes.forEach(function (node) {
+        if (node.nodeType !== 1) return;
+        var list = node.matches && node.matches('.profile-icon-btn[title]') ? [node] : [];
+        if (node.querySelectorAll) list = list.concat(Array.prototype.slice.call(node.querySelectorAll('.profile-icon-btn[title]')));
+        list.forEach(function (el) {
+          var t = el.getAttribute('title');
+          if (t) {
+            el.setAttribute('data-tooltip', t);
+            el.removeAttribute('title');
+          }
+        });
+      });
+    });
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
